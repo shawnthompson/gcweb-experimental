2ab66221b22b05f07bb571bcde8f65f123fd4419
Shawn Thompson <plansmash@me.com>
2016-04-21 14:25:06 -0400

    added assemble grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -4,6 +4,26 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 
+		assemble: {
+			options:
+				prettify:
+					indent: 2
+				marked:
+					sanitize: false
+				production: false
+				data: "site/data/**/*.{yml,json}"
+				helpers: "site/helpers/helper{,s}-*.js"
+				partials: "site/includes/**/*.hbs"
+				layoutdir: "site/layouts"
+				layout: "default.hbs"
+				assets: "dist/unmin"
+			},
+			site: {
+				src: ['site/*.hbs'],
+				dest: './dist'
+			}
+		},
+
 		'gh-pages': {
 			options: {
 				base: 'dist'
@@ -17,3 +37,8 @@ module.exports = function(grunt) {
 			'gh-pages'
 	]);
 };
+
+
+
+
+
